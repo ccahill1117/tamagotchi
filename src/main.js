3a456f7b208ad0f3cb3a521897462da1b77a465d
Christopher Cahill <cthomascahill@gmail.com>
2019-01-14 17:55:41 -0800

    auto increment ids for tamas

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
 import './styles.css';
 import { Tamagotchi } from './tamagotchi.js';
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 $(document).ready(function(){
+
+
   $("#inputForm").submit(function(event){
     event.preventDefault();
+
+    const updateDisplay = setInterval(() => {
+      update();
+    },300)
+
+    let update = function() {
+      $('.outputDiv').text(`newThing.food`);
+    }
 
     let userName = $("#inputText").val();
     let newThing = new Tamagotchi(userName);
     $(".resultDiv").empty();
     $(".resultDiv").text(newThing.name);
 
+    updateDisplay();
+
   });
+
 });
--- src/tamagotchi.js
+++ src/tamagotchi.js
@@ -1,3 +1,18 @@
 export function Tamagotchi(name) {
   this.name = name;
+  this.id = Tamagotchi.prototype.incrementID();
+  this.food = 100;
+  this.sleep = 100;
+  this.play = 100;
+  this.overTime = setInterval(() => {
+    this.food--;
+    this.sleep--;
+    this.boredom--;
+  }, 2000)
+}
+
+Tamagotchi.prototype.incrementID = function() {
+    if (!this.latestId) this.latestId = 1
+    else this.latestId++
+    return this.latestId
 }
